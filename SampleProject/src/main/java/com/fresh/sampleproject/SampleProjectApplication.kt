9e074e70e61dc1d90db5b01c1a5b42c9994d61b4
Lukas Roberts <lukas.roberts.90@googlemail.com>
2023-09-29 22:13:36 +0200

    Removed hilt

--- SampleProject/src/main/java/com/fresh/sampleproject/SampleProjectApplication.kt
+++ SampleProject/src/main/java/com/fresh/sampleproject/SampleProjectApplication.kt
@@ -1,8 +1,6 @@
 package com.fresh.sampleproject
 
 import android.app.Application
-//import dagger.hilt.android.HiltAndroidApp
 
-//@HiltAndroidApp
 class SampleProjectApplication : Application() {
 }
