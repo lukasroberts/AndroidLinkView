34820a8e2101c30fcfb258bdc88d37cd6971fd97
Lukas Roberts <lukas.roberts.90@googlemail.com>
2023-09-30 08:21:35 +0200

    Task/update to latest deps (#1)
    * Updating gradle wrapper
    
    * Updating most versions of libs
    
    * Removing hilt temporarily - bumping versions, all compiling
    
    * Library upgraded, now move onto sampleapp
    
    * Sample app compiling with latest deps, time to add back in Hilt
    
    * Removed hilt
    
    * bump jdk
    
    * bump versions

--- SampleProject/src/main/java/com/fresh/sampleproject/MainActivity.kt
+++ SampleProject/src/main/java/com/fresh/sampleproject/MainActivity.kt
@@ -3,33 +3,39 @@ package com.fresh.sampleproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.fresh.materiallinkpreview.models.OpenGraphMetaData
+import com.fresh.materiallinkpreview.parsing.OpenGraphMetaDataProvider
 import com.fresh.materiallinkpreview.ui.CardLinkPreview
 import com.fresh.materiallinkpreview.ui.CardLinkPreviewProperties
 import com.fresh.sampleproject.ui.theme.MaterialLinkPreviewTheme
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val viewModel: MainActivityViewModel by viewModels()
+    private val viewModel: MainActivityViewModel = MainActivityViewModel(OpenGraphMetaDataProvider())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
